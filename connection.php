894dfdd590e4185f5044c64d7389a6bb54bbeb2b
TheRebooters <149248590+TheRebooters@users.noreply.github.com>
2023-12-10 21:53:21 +0800

    Delete connection.php

--- connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-	// $conn = new mysqli('<server-name>', '<username>', '<password>', '<db-name>');
-	$conn = new mysqli('localhost', 'id21649396_attendance', 'Therebooters2023!', 'id21649396_attendanceqr');
-?>
