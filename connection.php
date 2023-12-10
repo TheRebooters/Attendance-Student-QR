9f88920fa583132084bb901f25f76c43c8e03625
TheRebooters <149248590+TheRebooters@users.noreply.github.com>
2023-12-10 22:34:36 +0800

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,4 @@
+<?php
+	// $conn = new mysqli('<server-name>', '<username>', '<password>', '<db-name>');
+	$conn = new mysqli('localhost', 'id21649396_attendance', 'Therebooters2023!', 'id21649396_attendanceqr');
+?>
